c32cecdc0e89f668114490302e3b4d2ab55a00bb
Cordial <alicia.cordial@laplateforme.io>
2021-09-13 13:52:14 +0200

    job 01 validé ok

---
+++ job01/calculator9000/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function TheTitle() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Calculator 9000</h1>
+      </header>
+    </div>
+  );
+}
+
+export default TheTitle;
